b893af7f8bf1f7c6aecca7a88f77e2e7d7fefaaa
IAleks94 <andrei@myrror.security>
2024-09-17 18:44:06 +0200

    fix tests

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { RetryService } from './retry.service';
 export { AuthService } from './auth.service';
 export { IssuesService } from './issues.service';
+export { ReportService } from './report.service';
 export { WebhookService } from './webhook.service';
